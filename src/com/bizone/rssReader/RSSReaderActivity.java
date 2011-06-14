46c1da8bf4ab9052d91f617a4cbb00fd2a92593c
jung-kyoon lee <excgate@gmail.com>
2011-06-14 18:53:55 +0900

    first commit

---
+++ src/com/bizone/rssReader/RSSReaderActivity.java
@@ -0,0 +1,15 @@
+package com.bizone.rssReader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RSSReaderActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
